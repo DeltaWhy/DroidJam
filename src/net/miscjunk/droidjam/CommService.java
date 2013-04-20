26ab8d20c1ef2bc308218cd2afc7af75580cb621
Michael Limiero <mike5713@gmail.com>
2013-04-20 07:59:02 -0400

    Fix up CommService

--- src/net/miscjunk/droidjam/CommService.java
+++ src/net/miscjunk/droidjam/CommService.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -16,68 +17,121 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.util.Log;
+
 public class CommService {
+    /* Usage example:
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);  
+        JSONArray a = (new CommService()).getJSONArray("http://obsidian.miscjunk.net:9393/bands");
+        Log.d("JSON",a.toString());
+        JSONObject o = (new CommService()).getJSON("http://obsidian.miscjunk.net:9393/bands/ce37f138f04d854bbee0");
+        Log.d("JSON",o.toString());
+     */
 
-	public JSONObject getJSON(String url){
-		 StringBuilder builder = new StringBuilder();
-		    HttpClient client = new DefaultHttpClient();
-		    HttpGet httpGet = new HttpGet(url);
-		    try {
-		      HttpResponse response = client.execute(httpGet);
-		      StatusLine statusLine = response.getStatusLine();
-		      int statusCode = statusLine.getStatusCode();
-		      if (statusCode == 200) {
-		        HttpEntity entity = response.getEntity();
-		        InputStream content = entity.getContent();
-		        BufferedReader reader = new BufferedReader(new InputStreamReader(content));
-		        String line;
-		        while ((line = reader.readLine()) != null) {
-		          builder.append(line);
-		        }
-		      } else {
-		        //Log.e(ParseJSON.class.toString(), "Failed to download file");
-		      }
-		    } catch (ClientProtocolException e) {
-		      e.printStackTrace();
-		    } catch (IOException e) {
-		      e.printStackTrace();
-		    }
-		    JSONObject jObject = new JSONObject();
-		    try {
-				jObject = new JSONObject(builder.toString());				
-			} catch (JSONException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		    return jObject;
-		  }
+    public JSONObject getJSON(String url){
+        String jsonString = getString(url);
+
+        JSONObject jObject = null;
+        if (jsonString != null) {
+            try {
+                jObject = new JSONObject(jsonString);				
+            } catch (JSONException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        return jObject;
+    }
+
+    public JSONArray getJSONArray(String url){
+        String jsonString = getString(url);
+
+        JSONArray jArray = null;
+        if (jsonString != null) {
+            try {
+                jArray = new JSONArray(jsonString);				
+            } catch (JSONException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        return jArray;
+    }
+
+    public String getString(String url){
+        StringBuilder builder = new StringBuilder();
+        HttpClient client = new DefaultHttpClient();
+        HttpGet httpGet = new HttpGet(url);
+        try {
+            HttpResponse response = client.execute(httpGet);
+            StatusLine statusLine = response.getStatusLine();
+            int statusCode = statusLine.getStatusCode();
+            HttpEntity entity = response.getEntity();
+            InputStream content = entity.getContent();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                builder.append(line);
+            }
+            return builder.toString();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 	
-	public JSONObject postJSON(String url, JSONObject jObject) throws Exception 
-	{
-	    //instantiates httpclient to make request
-	    DefaultHttpClient httpclient = new DefaultHttpClient();
-
-	    //url with the post data
-	    HttpPost httpost = new HttpPost(url);
-
-	    //passes the results to a string builder/entity
-	    StringEntity se = new StringEntity(jObject.toString());
-
-	    //sets the post request as the resulting string
-	    httpost.setEntity(se);
-	    //sets a request header so the page receving the request
-	    //will know what to do with it
-	    httpost.setHeader("Accept", "application/json");
-	    httpost.setHeader("Content-type", "application/json");
-
-	    //Handles what is returned from the page 
-	    ResponseHandler responseHandler = new BasicResponseHandler();
-	    return httpclient.execute(httpost, responseHandler);
-	}
 	
+    public JSONObject postJSON(String url, JSONObject jRequest)
+    {
+        //instantiates httpclient to make request
+        DefaultHttpClient httpclient = new DefaultHttpClient();
 
-	
+        //url with the post data
+        HttpPost httpost = new HttpPost(url);
+
+        //passes the results to a string builder/entity
+        StringEntity se;
+        try {
+            se = new StringEntity(jRequest.toString());
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        //sets the post request as the resulting string
+        httpost.setEntity(se);
+        //sets a request header so the page receving the request
+        //will know what to do with it
+        httpost.setHeader("Accept", "application/json");
+        httpost.setHeader("Content-type", "application/json");
+
+        //Handles what is returned from the page 
+        ResponseHandler responseHandler = new BasicResponseHandler();
+        String jsonString = null;
+        try {
+            jsonString = httpclient.execute(httpost, responseHandler);
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        JSONObject jResponse = null;
+        if (jsonString != null) {
+            try {
+                jResponse = new JSONObject(jsonString);				
+            } catch (JSONException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        return jResponse;
+    }
 }
