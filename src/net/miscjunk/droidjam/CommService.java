d79d252b088caa6fc643f3d60a293967c277747d
Atom Raiff <atom.raiff@live.com>
2013-04-20 05:54:57 -0400

    added drum .wav's

--- src/net/miscjunk/droidjam/CommService.java
+++ src/net/miscjunk/droidjam/CommService.java
@@ -1,5 +1,59 @@
 package net.miscjunk.droidjam;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class CommService {
 
+	public String restGet(String url){
+		 StringBuilder builder = new StringBuilder();
+		    HttpClient client = new DefaultHttpClient();
+		    HttpGet httpGet = new HttpGet(url);
+		    try {
+		      HttpResponse response = client.execute(httpGet);
+		      StatusLine statusLine = response.getStatusLine();
+		      int statusCode = statusLine.getStatusCode();
+		      if (statusCode == 200) {
+		        HttpEntity entity = response.getEntity();
+		        InputStream content = entity.getContent();
+		        BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+		        String line;
+		        while ((line = reader.readLine()) != null) {
+		          builder.append(line);
+		        }
+		      } else {
+		        //Log.e(ParseJSON.class.toString(), "Failed to download file");
+		      }
+		    } catch (ClientProtocolException e) {
+		      e.printStackTrace();
+		    } catch (IOException e) {
+		      e.printStackTrace();
+		    }
+		    return builder.toString();
+		  }
+	
+	public JSONObject writeJSON() {
+		  JSONObject object = new JSONObject();
+//		  try {
+//		    object.put("name", "Jack Hack");
+//		    object.put("score", new Integer(200));
+//		    object.put("current", new Double(152.32));
+//		    object.put("nickname", "Hacker");
+//		  } catch (JSONException e) {
+//		    e.printStackTrace();
+//		  }
+		  return object;
+		} 
 }
